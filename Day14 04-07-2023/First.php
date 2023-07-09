84fd226177d2c450971dc4cf68aa801810a730ff
Maury Soham YogeshKumar <137367860+Itzsoham@users.noreply.github.com>
2023-07-10 00:13:51 +0530

    Add files via upload

---
+++ Day14 04-07-2023/First.php
@@ -0,0 +1,8 @@
+<body>
+    <?php 
+        echo "Hello!"."<br>";
+        print "How are you?"."<br>";
+        echo "I am fine, ";
+        print "thank you.";
+    ?>
+</body>
---
+++ Day14 04-07-2023/Work.php
@@ -0,0 +1,8 @@
+<body>
+    <?php 
+        echo "Hello!"."<br>";
+        print "We install xampp";
+        echo "It didn't work,"."<br>";
+        print "So, we install wamp and run code";
+    ?>
+</body>
