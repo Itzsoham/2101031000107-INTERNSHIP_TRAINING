e1729500ca5abfa082b273bb498d71f234bc317f
Maury Soham YogeshKumar <137367860+Itzsoham@users.noreply.github.com>
2023-07-10 15:26:20 +0530

    Add files via upload

---
+++ Day19 10-07-2023/Employe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Employee Details");
+
+            Console.WriteLine("Enter Employee ID");
+            int EmployeeID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter Employee Salary");
+            int Salary = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Address");
+            string Address = Console.ReadLine();
+            Console.WriteLine("Enter Employee Department");
+            string Department = Console.ReadLine();
+
+   
+            Console.WriteLine("\nEntered Employee Details are as Follows:");
+            Console.WriteLine($"Employee ID Is: {EmployeeID}");
+            Console.WriteLine($"Employee Name Is: {Name}");
+            Console.WriteLine($"Employee Salary Is: {Salary}");
+            Console.WriteLine($"Employee Address Is: {Address}");
+            Console.WriteLine($"Employee Department Is: {Department}");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
