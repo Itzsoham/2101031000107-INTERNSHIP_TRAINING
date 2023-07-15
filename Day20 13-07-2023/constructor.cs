e28a7b22609d5efc2556345820b8b1b9009e18f2
Maury Soham YogeshKumar <137367860+Itzsoham@users.noreply.github.com>
2023-07-15 11:16:39 +0530

    Add files via upload

---
+++ Day20 13-07-2023/constructor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ConstructorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExplicitConstructor obj = new ExplicitConstructor();
+
+            Console.ReadKey();
+        }
+    }
+    class ExplicitConstructor
+    {
+        public ExplicitConstructor()
+        {
+            Console.WriteLine("Explicit Constructor is Called!");
+        }
+    }
+}
---
+++ Day20 13-07-2023/overload.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace MethodOverloading
+{
+    class overload
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Add(float x, float y)
+        {
+            Console.WriteLine(x + y);
+        }
+        public void Add(string s1, string s2)
+        {
+            Console.WriteLine(s1 + " " + s2);
+        }
+        static void Main(string[] args)
+        {
+            overload obj = new overload();
+            obj.Add(10, 20);
+            obj.Add(10.5f, 20.5f);
+            obj.Add("Pranaya", "Rout");
+            Console.ReadKey();
+        }
+    }
+}
